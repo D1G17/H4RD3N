c6a05f04e82b6614b7e1336959ed0d8b745614de
d1g <156820616+D1G17@users.noreply.github.com>
2025-09-03 17:33:34 +0200

    Update iSPI.sh
    include tun+ (L3) VPN interface class besides L2 tap+

--- iSPI.sh
+++ iSPI.sh
@@ -3,8 +3,8 @@
 iptables -I INPUT -m state --state related,established -j ACCEPT
 # Allow traffic to SSH (as an anti-lockout rule)
 iptables -I INPUT -p tcp --dport 22 -j ACCEPT
-# Allow traffic via VPN (OpenVPN, wireguard) and loopback interfaces
-for i in tap+ wg+ lo; do iptables -I INPUT -i $i -j ACCEPT; done
+# Allow traffic via VPN and loopback interfaces
+for i in tun+ tap+ wg+ lo; do iptables -I INPUT -i $i -j ACCEPT; done
 # Log packets that do not match previous rules
 iptables -A INPUT -j LOG --log-prefix `hostname`-INPUT
 # Block packets that do not match previous rules

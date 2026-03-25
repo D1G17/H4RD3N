4dced66880e4108849bb80fb74727bf97c53b9ac
d1g <156820616+D1G17@users.noreply.github.com>
2026-03-25 21:43:50 +0100

    Update iSPI.sh

--- iSPI.sh
+++ iSPI.sh
@@ -2,22 +2,21 @@
 #
 # Stateful Packet Inspection (SPI) FW Ruleset
 #
+# Dropped packets can be observed and rules created
+# Rules can be saved e.g. via netfilter-persistent save
 #
-# Hint: Rules can be saved e.g. via netfilter-persistent save
-# Implement SPI for all packets reaching INPUT
+# * Implement SPI for all packets reaching INPUT and FORWARD
+# * Allow traffic to SSH (as an anti-lockout rule)
+# * Allow traffic via VPN and loopback interfaces
+# * Log packets that do not match previous INPUT and FORWARD rules
+# * Block packets that do not match previous rules by implementing DROP policy
+#
 iptables -I INPUT -m state --state related,established -j ACCEPT
-# Allow traffic to SSH (as an anti-lockout rule)
 iptables -I INPUT -p tcp --dport 22 -j ACCEPT
-# Allow traffic via VPN and loopback interfaces
 for i in tun+ tap+ wg+ lo; do iptables -I INPUT -i $i -j ACCEPT; done
-# Log packets that do not match previous INPUT rules
 iptables -A INPUT -j LOG --log-prefix `hostname`-INPUT
-# Block packets that do not match previous rules by implementing DROP policy
 iptables -A INPUT -P DROP
-# Implement SPI for all packets reaching FORWARD
 iptables -I FORWARD -m state --state related,established -j ACCEPT
-# Log packets that do not match previous FORWARD rules
 iptables -A FORWARD -j LOG --log-prefix `hostname`-INPUT
-# Block packets that do not match previous rules by imlementing DROP policy
 iptables -A FORWARD -P DROP
 exit 0

de2db3c92db94bf32d38e114d6611080455906cc
d1g <156820616+D1G17@users.noreply.github.com>
2026-09-14 15:09:25 +0200

    Update iSPI.sh

--- iSPI.sh
+++ iSPI.sh
@@ -2,7 +2,7 @@
 #
 # Stateful Packet Inspection (SPI) FW Ruleset
 #
-# Dropped packets can be observed and rules created
+# Dropped packets can be observed
 # Rules can be saved e.g. via netfilter-persistent save
 #
 # * Implement SPI for all packets reaching INPUT and FORWARD
@@ -11,13 +11,14 @@
 # * Log packets that do not match previous INPUT and FORWARD rules
 # * Block packets that do not match previous rules by implementing DROP policy
 #
-echo "Imlementing SPI ruleset..."
+echo "++ Imlementing SPI ruleset..."
 iptables -I INPUT -m state --state related,established -j ACCEPT
-iptables -I INPUT -p tcp --dport 22 -j ACCEPT
-for i in tun+ tap+ wg+ lo; do iptables -I INPUT -i $i -j ACCEPT; done
-iptables -A INPUT -j LOG --log-prefix `hostname`-INPUT
 iptables -I FORWARD -m state --state related,established -j ACCEPT
-iptables -A FORWARD -j LOG --log-prefix `hostname`-INPUT
+iptables -I INPUT -p tcp --dport 22 -j ACCEPT
+echo "++ Implement Logging..."
+for i in tun+ tap+ wg+ lo; do iptables -I INPUT -i $i -j ACCEPT; done
+for i in INPUT FORWARD; do iptables -A $i -j LOG --log-prefix `hostname`-$i
+echo "++ Set Policy..."
 for i in INPUT FORWARD; do iptables -P $i DROP ; done
-echo "Implemented SPI rules OK+"
+echo "++ Implemented SPI rules OK+"
 exit 0

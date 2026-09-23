11b99d72c20bd101137e929d0671a3b13db8d885
d1g <156820616+D1G17@users.noreply.github.com>
2026-09-23 17:21:09 +0200

    Update iSPI.sh

--- iSPI.sh
+++ iSPI.sh
@@ -2,9 +2,8 @@
 #
 # Stateful Packet Inspection (SPI) FW Ruleset
 #
-# Dropped packets can be observed
-# Rules can be saved e.g. via netfilter-persistent save
-#
+# * Dropped packets can be observed
+# * Rules can be saved e.g. via netfilter-persistent save
 # * Implement SPI for all packets reaching INPUT and FORWARD
 # * Allow traffic to SSH (as an anti-lockout rule)
 # * Allow traffic via VPN and loopback interfaces
@@ -15,9 +14,9 @@ echo "++ Imlementing SPI ruleset..."
 iptables -I INPUT -m state --state related,established -j ACCEPT
 iptables -I FORWARD -m state --state related,established -j ACCEPT
 iptables -I INPUT -p tcp --dport 22 -j ACCEPT
-echo "++ Implement Logging..."
 for i in tun+ tap+ wg+ lo; do iptables -I INPUT -i $i -j ACCEPT; done
-for i in INPUT FORWARD; do iptables -A $i -j LOG --log-prefix `hostname`-$i
+echo "++ Implement Logging..."
+for i in INPUT FORWARD; do iptables -A $i -j LOG --log-prefix `hostname`-$i; done
 echo "++ Set Policy..."
 for i in INPUT FORWARD; do iptables -P $i DROP ; done
 echo "++ Implemented SPI rules OK+"
